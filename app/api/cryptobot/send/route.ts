2370bee27ca758acd511d95288dddc5450e317f1
Anthony Kobzar <anthonykobzar@berkeley.edu>
2025-10-26 08:12:38 -0700

    api

--- app/api/cryptobot/send/route.ts
+++ app/api/cryptobot/send/route.ts
@@ -6,11 +6,14 @@ import { NextResponse } from 'next/server';
 import { sendSUI } from '../../../../scripts/testSend';
 // Use Node.js runtime for crypto compatibility
 export const runtime = 'nodejs';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../../../.env.local' , override: true });
 
 export async function POST(request: Request) {
   try {
+    const priv = process.env.CRYPTOBOT_PRIVATE_KEY;
     const res = await sendSUI(
-      "",
+      priv,
       "0x3f6bb1bdaaacafd020194d452a5a1afce89114cd5fafa3aebc9b214e83aa2ef2",
       0.001
     );
